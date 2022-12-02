4f01666847c2c93e3112e75498380ee75fb71913
BaileyMagee <wolf217c@gmail.com>
2022-12-02 16:21:11 -0600

    Phase-2

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,5 +1,8 @@
 -- Find Hermione's cats
 -- Your code here
+SELECT cats.name, owners.first_name FROM cat_owners
+JOIN cats ON (cat_owners.cat_id = cats.id)
+JOIN owners ON (cat_owners.owner_id = owners.id);
 
 -- Find All the Toys for Hermione's cats
 -- Your code here
